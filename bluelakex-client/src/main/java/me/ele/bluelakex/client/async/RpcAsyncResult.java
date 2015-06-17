13fd4eeb258e69d42cd1a4e7f0924ae1c32cc566
zhanghua <zhanghua@172-13-1-138.lightspeed.mssnks.sbcglobal.net>
2015-06-17 14:17:00 +0800

    Add Cglib ProxyClass Cache

--- bluelakex-client/src/main/java/me/ele/bluelakex/client/async/RpcAsyncResult.java
+++ bluelakex-client/src/main/java/me/ele/bluelakex/client/async/RpcAsyncResult.java
@@ -1,13 +1,17 @@
 package me.ele.bluelakex.client.async;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.LazyLoader;
 import me.ele.bluelakex.model.ResponseEntity;
+import me.ele.bluelakex.utils.ReflectionUtils;
 
 public class RpcAsyncResult {
 	
@@ -16,19 +20,32 @@ public class RpcAsyncResult {
     private Class<?>  returnClass;
     private Future<ResponseEntity> future;
     
+    private static Map<String,Class<?>> proxyCache = new HashMap<String,Class<?>>();
+    
 	public RpcAsyncResult(Class<?> returnClass, Future<ResponseEntity> future) {
 		this.returnClass = returnClass;
 		this.future = future;
 	}
     
 	public Object getProxy() {
-		Enhancer enhancer = new Enhancer();
-		if (returnClass.isInterface()) 
-			enhancer.setInterfaces(new Class[] {returnClass});
-		else
-			enhancer.setSuperclass(returnClass);
-		enhancer.setCallback(new AsyncLoadResultInterceptor());
-		return enhancer.create();
+		Class<?> proxyClass = proxyCache.get(returnClass.getName());
+		if(proxyClass == null){
+			Enhancer enhancer = new Enhancer();
+			if (returnClass.isInterface()) 
+				enhancer.setInterfaces(new Class[] {returnClass});
+			else
+				enhancer.setSuperclass(returnClass);
+			enhancer.setCallbackType(AsyncLoadResultInterceptor.class);
+			proxyClass = enhancer.createClass();
+			proxyCache.put(returnClass.getName(), proxyClass);
+		}
+		Enhancer.registerCallbacks(proxyClass, new Callback[] {new AsyncLoadResultInterceptor()});
+		try{
+			return ReflectionUtils.newInstance(proxyClass);
+		}
+		finally{
+			Enhancer.registerCallbacks(proxyClass, null);
+		}
 	}
 	
 	class AsyncLoadResultInterceptor implements LazyLoader {
